cdb1a6bb6506dc14bc7db286db57bbea944b0c9b
thuanphat <ctphat@gimasys.com>
2025-12-23 17:10:38 +0700

    .

--- src/controllers/guest/GuestController.js
+++ src/controllers/guest/GuestController.js
@@ -88,45 +88,41 @@ class GuestController {
             if (req.data.access_token) {
                 const accessToken = req.data.access_token;
                 console.log(accessToken);
-                let data = '';
+                // let data = '';
 
-                let config2 = {
-                    method: 'GET',
-                    maxBodyLength: Infinity,
-                    url: `${process.env.SF_HOST}/services/data/v65.0/ssot/insight/calculated-insights/web_catalog_insight_realtime__cio`,
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                    data: data,
-                };
-                const resInsight = await axios.request(config2);
-                console.log(resInsight.data);
-                if (resInsight.data.data.length > 0) {
-                    console.log('here');
-                    if (webhookData.events && webhookData.events.length > 0) {
-                        console.log('here2');
-                        for (const eventItem of webhookData.events) {
-                            try {
-                                const rawPayloadString = eventItem.PayloadCurrentValue;
-                                const parsedBody = JSON.parse(rawPayloadString);
-                                const deviceId = parsedBody['Website_Connection_Behavioral_E_2656__dlm_deviceId__c'];
-                                const catalogId = parsedBody['Website_Connection_Behavioral_E_2656__dlm_catalog_id__c'];
-                                if (deviceId && catalogId) {
-                                    console.log('here3');
-
-                                    const result = resInsight.data.find(
-                                        (item) =>
-                                            item.data_graph_dimension__c === deviceId &&
-                                            item.productid__c === catalogId,
-                                    );
-                                    console.log('result:', result);
-                                    ReqDiscountProduct(deviceId, result);
-                                }
-                            } catch (err) {
-                                console.error('Lỗi khi xử lý một event trong batch:', err);
+                // let config2 = {
+                //     method: 'get',
+                //     maxBodyLength: Infinity,
+                //     url: `${process.env.SF_HOST}/services/data/v65.0/ssot/insight/calculated-insights/web_catalog_insight_realtime__cio`,
+                //     headers: {
+                //         Authorization: `Bearer ${accessToken}`,
+                //     },
+                //     data: data,
+                // };
+                // const resInsight = await axios.request(config2);
+                // console.log(resInsight.data);
+                // if (resInsight.data.data.length > 0) {
+                if (webhookData.events && webhookData.events.length > 0) {
+                    for (const eventItem of webhookData.events) {
+                        try {
+                            const rawPayloadString = eventItem.PayloadCurrentValue;
+                            const parsedBody = JSON.parse(rawPayloadString);
+                            const deviceId = parsedBody['Website_Connection_Behavioral_E_2656__dlm_deviceId__c'];
+                            const catalogId = parsedBody['Website_Connection_Behavioral_E_2656__dlm_catalog_id__c'];
+                            if (deviceId && catalogId) {
+                                // const result = resInsight.data.find(
+                                //     (item) =>
+                                //         item.data_graph_dimension__c === deviceId &&
+                                //         item.productid__c === catalogId,
+                                // );
+                                // console.log('result:', result);
+                                ReqDiscountProduct(deviceId, catalogId);
                             }
+                        } catch (err) {
+                            console.error('Lỗi khi xử lý một event trong batch:', err);
                         }
                     }
+                    // }
                     return res.status(200).json({ message: 'Success' });
                 } else {
                     return res.status(500).json({ error: 'internal_error' });
